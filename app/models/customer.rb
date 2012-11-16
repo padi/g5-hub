2a038a9976fcd15311df5279ea519c0639e236e3
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-15 20:56:26 -0800

    removes references to admin

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,6 @@
 class Customer < ActiveRecord::Base
-  attr_accessible :admin_id, :name, :locations_attributes, :feature_ids
+  attr_accessible :name, :locations_attributes, :feature_ids
 
-  belongs_to :admin
   has_many :locations
   has_and_belongs_to_many :features
 
