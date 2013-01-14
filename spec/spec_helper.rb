8769b0ac0dda38eb9a5462f6ba8fdfe9f2992b8d
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-01-13 16:40:03 -0800

    updates spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,33 @@
-require 'simplecov'
-SimpleCov.start 'rails'
-
 require 'rubygems'
 require 'spork'
-#uncomment the following line to use spork with the debugger
-#require 'spork/ext/ruby-debug'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
-
 
 Spork.prefork do
+  unless ENV['DRB']
+    require 'simplecov'
+    SimpleCov.start 'rails'
+  end
+
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
   RSpec.configure do |config|
     config.use_transactional_fixtures = true
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
   end
+
+  Spork.trap_method(Rails::Application, :eager_load!)
+  require File.expand_path("../../config/environment", __FILE__)
+  Rails.application.railties.all { |r| r.eager_load! }
 end
 
 Spork.each_run do
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
+  if ENV['DRB']
+    require 'simplecov'
+    SimpleCov.start 'rails'
+  end
 end
