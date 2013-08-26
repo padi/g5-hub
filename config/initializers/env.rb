aa7f5b210c6e3a4a6a7a0cd9fc8c040ace4abc81
Don Petersen <don@donpetersen.net>
2013-08-26 19:14:35 +0000

    Don't overwrite the G5_CONFIGURATOR_WEBHOOK_URL.
    The README says you can set this environment variable and that an
    initializer sets some defaults, but the initializer doesn't check to see
    if the variable is defined before smashing it.

--- config/initializers/env.rb
+++ config/initializers/env.rb
@@ -1,4 +1,4 @@
-ENV["G5_CONFIGURATOR_WEBHOOK_URL"] = case Rails.env
+ENV["G5_CONFIGURATOR_WEBHOOK_URL"] ||= case Rails.env
   when "production"  then "http://g5-configurator.herokuapp.com/consume_feed"
   when "development" then "http://g5-configurator.dev/consume_feed"
   when "test"        then "http:://g5-configurator.test/consume_feed"
