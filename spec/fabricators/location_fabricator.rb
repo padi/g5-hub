06a478f3b20c5b7c073686c059f2ff4b10b44b1e
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-02-28 13:09:36 -0800

    Update Location fabricator to satisfy validations

--- spec/fabricators/location_fabricator.rb
+++ spec/fabricators/location_fabricator.rb
@@ -1,3 +1,9 @@
 Fabricator(:location) do
   name { Faker::Name.name }
+  domain { Faker::Internet.domain_name }
+  street_address_1 { Faker::Address.street_address }
+  city { Faker::Address.city }
+  state { Faker::Address.state }
+  postal_code { Faker::Address.zip_code }
+  phone_number { Faker::PhoneNumber.phone_number }
 end
