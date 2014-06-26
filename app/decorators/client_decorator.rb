44967f9364b61161a79b6fa8cee8b8e96e02b9ce
Michael Mitchell <michael.mitchell@getg5.com>
2014-06-26 08:34:36 -0700

    decorates all on client and location decorators

--- app/decorators/client_decorator.rb
+++ app/decorators/client_decorator.rb
@@ -1,8 +1,8 @@
 class ClientDecorator < Draper::Decorator
   include HentryableDates
 
-  delegate :name, :street_address_1, :street_address_2, :city, :state,
-           :postal_code, :fax, :email, :vertical, :urn, :domain_type, :domain
+  delegate_all  
   decorates_association :locations, with: LocationDecorator
+
 end
 
