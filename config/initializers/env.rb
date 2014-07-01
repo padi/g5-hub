4ce540922d51173fbd3dd7bab54644d3055f883c
bbauer <brian.bauer@g5platform.com>
2014-07-01 14:04:27 -0700

    Use custom path for cms

--- config/initializers/env.rb
+++ config/initializers/env.rb
@@ -4,6 +4,12 @@ ENV["G5_CONFIGURATOR_WEBHOOK_URL"] ||= case Rails.env
   when "test"        then "http:://g5-configurator.test/consume_feed"
 end
 
+ENV["CMS_UPDATE_PATH"] ||= case Rails.env
+  when "production"  then  "/update"
+  when "development" then  "/foo"
+  when "test"        then  "/foo"
+end
+
 ENV["G5_UPDATABLE_PATH"] ||= case Rails.env
   when "production"  then  "/g5_updatable/update"
   when "development" then  "/foo"
