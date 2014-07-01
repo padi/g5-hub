0c4cce8e7ba1d3d775fc4d8466dc961a4e8821eb
bbauer <brian.bauer@g5platform.com>
2014-07-01 13:59:53 -0700

    Update webhook path for g5_updatable apps

--- config/initializers/env.rb
+++ config/initializers/env.rb
@@ -4,20 +4,8 @@ ENV["G5_CONFIGURATOR_WEBHOOK_URL"] ||= case Rails.env
   when "test"        then "http:://g5-configurator.test/consume_feed"
 end
 
-ENV["CMS_UPDATE_PATH"] ||= case Rails.env
-  when "production"  then  "/update"
-  when "development" then  "/foo"
-  when "test"        then  "/foo"
-end
-
-ENV["CPNS_UPDATE_PATH"] ||= case Rails.env
-  when "production"  then  "/update"
-  when "development" then  "/foo"
-  when "test"        then  "/foo"
-end
-
-ENV["CPAS_UPDATE_PATH"] ||= case Rails.env
-  when "production"  then  "/update"
+ENV["G5_UPDATABLE_PATH"] ||= case Rails.env
+  when "production"  then  "/g5_updatable/update"
   when "development" then  "/foo"
   when "test"        then  "/foo"
 end
