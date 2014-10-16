47ed8e4989e8f64292c9b5273554dd82dd473f59
Michael Mitchell <michael.mitchell@getg5.com>
2014-10-16 10:10:03 -0700

    adds host config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,6 @@ G5Hub::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  Rails.application.default_url_options[:host] = "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
 end
+
