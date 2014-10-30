4d4d744feadedf08dc9af75539e024365b54caf9
johnlucia <johnmlucia@gmail.com>
2014-10-30 12:23:11 -0700

    tweak da jsno

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -72,8 +72,9 @@ class ClientsController < ApplicationController
     radius_search = RadiusSearch.new(@client, params)
     @locations = radius_search.locations
 
-    render json: @locations.as_json(only: [:id, :name, :street_address_1, :street_address_2,
-      :city, :state, :postal_code, :email, :latitude, :longitude])
+    response = { locations: @locations.as_json(only: [:id, :name, :street_address_1, :street_address_2, :city, :state, :postal_code, :email, :latitude, :longitude]) }
+
+    render json: response
   end
 
   private
