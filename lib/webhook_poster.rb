762472b2d3aaa71ee7ad89e19c05e27f8b2fcb38
Perry Hertler <perry@hertler.org>
2014-11-04 15:07:22 -0600

    webhooking inventory-etl & vendor-leads

--- lib/webhook_poster.rb
+++ lib/webhook_poster.rb
@@ -1,9 +1,9 @@
 class WebhookPoster
   HEROKU_APP_NAME_MAX_LENGTH = 30
-  CMS_RECORD_TYPE  = ENV["APP_NAMESPACE"] + "-cms"
-  CPAS_RECORD_TYPE = ENV["APP_NAMESPACE"] + "-cpas"
-  CPNS_RECORD_TYPE = ENV["APP_NAMESPACE"] + "-cpns"
-  JOBS_RECORD_TYPE = ENV["APP_NAMESPACE"] + "-jobs"
+  CMS_RECORD_TYPE            = ENV["APP_NAMESPACE"] + "-cms"
+  CPAS_RECORD_TYPE           = ENV["APP_NAMESPACE"] + "-cpas"
+  CPNS_RECORD_TYPE           = ENV["APP_NAMESPACE"] + "-cpns"
+  JOBS_RECORD_TYPE           = ENV["APP_NAMESPACE"] + "-jobs"
 
   def initialize(client)
     @client = client
@@ -25,27 +25,42 @@ class WebhookPoster
     Rails.logger.info("posting to #{domain_for(CPNS_RECORD_TYPE)}#{ENV['G5_UPDATABLE_PATH']}")
     post("#{domain_for(CPNS_RECORD_TYPE)}#{ENV["G5_UPDATABLE_PATH"]}", client_uid: client_uid)
 
-    unless jobs_url.blank?
-      Rails.logger.info("posting to #{jobs_url}/#{ENV['G5_UPDATABLE_PATH']}")
-      post("#{jobs_url}#{ENV['G5_UPDATABLE_PATH']}", client_uid: client_uid)
-    end
+    post_client_updates_to_centralized
   end
 
   private
 
   def client_uid
     "https://" +
-      (ENV["HEROKU_APP_NAME"] || "g5-hub") +
-      ".herokuapp.com" +
-      Rails.application.routes.url_helpers.client_path(@client)
+        (ENV["HEROKU_APP_NAME"] || "g5-hub") +
+        ".herokuapp.com" +
+        Rails.application.routes.url_helpers.client_path(@client)
   end
 
   def domain_for(type)
     "https://#{converted_urn(type)}.herokuapp.com"
   end
 
+  def post_client_updates_to_centralized
+    [:jobs_url, :inventory_etl_url, :vendor_leads_url].each do |url_method|
+      url = send(url_method)
+      unless url.blank?
+        Rails.logger.info("posting to #{url}/#{ENV['G5_UPDATABLE_PATH']}")
+        post("#{url}#{ENV['G5_UPDATABLE_PATH']}", client_uid: client_uid)
+      end
+    end
+  end
+
   def jobs_url
     ENV['JOBS_URL']
+  end
+
+  def inventory_etl_url
+    ENV['INVENTORY_ETL_URL']
+  end
+
+  def vendor_leads_url
+    ENV['VENDOR_LEADS_URL']
   end
 
   def post(url, params={})
